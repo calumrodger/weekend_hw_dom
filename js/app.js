bd36a2ece52cd3ae17e9beb311cc32181e7049be
calumrodger <94742384+calumrodger@users.noreply.github.com>
2022-02-13 10:37:31 +0000

    added js for mvp functions and fixed typo in html

--- js/app.js
+++ js/app.js
@@ -1,4 +1,43 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('JavaScript loaded');
+    const newGameForm = document.querySelector("#new-game-form");
+    newGameForm.addEventListener("submit", handleNewGameFormSubmit);
+
+    const deleteAllButton = document.querySelector("#delete-all");
+    deleteAllButton.addEventListener("click", handleDeleteAllClick);
   })
+
+  const handleNewGameFormSubmit = function (event) {
+      event.preventDefault();
+
+      const gameCollectionItem = createGameCollectionItem(event.target);
+      const gameCollection = document.querySelector("#game-collection");
+      gameCollection.appendChild(gameCollectionItem);
+
+      event.target.reset();
+  }
+
+  const createGameCollectionItem = function (form) {
+      const gameCollectionItem = document.createElement("li");
+      gameCollectionItem.classList.add("game-collection-item");
+
+      const title = document.createElement("h2");
+      title.textContent = form.title.value;
+      gameCollectionItem.appendChild(title);
+
+      const genre = document.createElement("h3");
+      genre.textContent = form.genre.value;
+      gameCollectionItem.appendChild(genre);
+
+      const system = document.createElement("p");
+      system.textContent = form.system.value;
+      gameCollectionItem.appendChild(system);
+
+      return gameCollectionItem;
+  }
+
+  const handleDeleteAllClick = function (event) {
+      const gameCollection = document.querySelector("#game-collection");
+      gameCollection.innerHTML = "";
+  }
